e2bde60c373377aeb7d7a73b1246aa1e69850cc5
malikkurosaki <kurosakiblackangel@gmail.com>
2021-12-28 11:49:10 +0800

    hapus

--- README.md
+++
@@ -1,4 +0,0 @@
-
-malikkurosaki
-"ayo di up"
-
---
+++ index.js
@@ -0,0 +1,8 @@
+const App = require("makuro_js/src/app");
+const Container = require("makuro_js/src/container");
+
+App({
+    home: Container({
+        
+    })
+})
